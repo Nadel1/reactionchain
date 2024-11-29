1a7822f9909b72a69f8fb3d978677c3f867e3714
Natalia <n.piotrowski@tum.de>
2024-11-29 12:15:51 +0100

    fix

--- Scripts/button.gd
+++ Scripts/button.gd
@@ -5,6 +5,7 @@ extends Node2D
 
 var goodHit=false
 var okayHit=false
+var lastButton=false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
