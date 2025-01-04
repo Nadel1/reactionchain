aab8271f8440f5ec18936ff927e47da2ae941860
Tec <lukasmoersler@gmail.com>
2025-01-04 13:19:58 +0100

    Make solos not happen 100% of the time

--- Scripts/Stream/eventScheduler.gd
+++ Scripts/Stream/eventScheduler.gd
@@ -1,20 +1,22 @@
 extends Node
 
-var eventChances = [0.5]
+var eventChances = 2 # Chance is "1 in [eventChances]"
 
 func generateEvents():
-	var newEvent = Event.new()
-	var length = randi_range(2,4)
-	# TODO: Find out why this breaks at 1 and 2
-	var insertAt = randi_range(3,Global.musicTracks[0].size()-1)
-	#print("Event at " + str(insertAt))
-	newEvent.length = length
-	newEvent.type = Event.Types.PAUSE_REWIND
-	newEvent.startIndex = insertAt
-	newEvent.startLayer = Global.currentStreamIndex
-	
-	#TODO: Variable chance of event happening in the first place
-	scheduleEvent(newEvent)
+	var coinflip = randi() % eventChances == 0
+	if coinflip:
+		var newEvent = Event.new()
+		var length = randi_range(2,4)
+		# TODO: Find out why this breaks at 1 and 2
+		var insertAt = randi_range(3,Global.musicTracks[0].size()-1)
+		#print("Event at " + str(insertAt))
+		newEvent.length = length
+		newEvent.type = Event.Types.PAUSE_REWIND
+		newEvent.startIndex = insertAt
+		newEvent.startLayer = Global.currentStreamIndex
+		
+		#TODO: Variable chance of event happening in the first place
+		scheduleEvent(newEvent)
 
 func scheduleEvent(newEvent : Event):
 	get_parent().insertEvent(newEvent)
