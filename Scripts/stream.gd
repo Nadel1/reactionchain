ea42086cfb5ff1e30f87c10de673c506a5601cfa
Natalia <n.piotrowski@tum.de>
2025-01-05 17:12:44 +0100

    integrate AI streamer

--- Scripts/stream.gd
+++ Scripts/stream.gd
@@ -14,7 +14,8 @@ const STREAMER0=preload("res://Scenes/Objects/Streamers/streamerBasic0.tscn")
 const STREAMER1=preload("res://Scenes/Objects/Streamers/streamerBasic1.tscn")
 const STREAMER2=preload("res://Scenes/Objects/Streamers/streamerBasic2.tscn")
 const STREAMERAI=preload("res://Scenes/Objects/Streamers/streamerAI.tscn")
-var allStreamers=[STREAMERAI]#STREAMER0, STREAMER1, STREAMER2]
+var allStreamers=[STREAMER0, STREAMER1, STREAMER2]
+var aiStreamer=[STREAMERAI]
 var currentStreamerIndex=0
 var currentStreamer=null
 
@@ -31,11 +32,14 @@ func _on_start_playing_music_timer_timeout() -> void:
 
 func prepareStreamer():
 	currentStreamerIndex = randi() % allStreamers.size()
-	if Global.streamerIndices.size()>0 and currentStreamerIndex==Global.streamerIndices[Global.currentStreamIndex-1]: #making sure we dont pick the same streamer twice in a row
-		currentStreamerIndex += 1
-		currentStreamerIndex %= allStreamers.size()
-		
-	currentStreamer=allStreamers[currentStreamerIndex].instantiate()
+	if Global.currentStreamIndex==0:
+		currentStreamer=aiStreamer[0].instantiate()
+	else: 
+		if Global.streamerIndices.size()>0 and currentStreamerIndex==Global.streamerIndices[Global.currentStreamIndex-1]: #making sure we dont pick the same streamer twice in a row
+			currentStreamerIndex += 1
+			currentStreamerIndex %= allStreamers.size()
+			
+		currentStreamer=allStreamers[currentStreamerIndex].instantiate()
 	currentStreamer.position=$UI/StreamerPlaceholder.position
 	currentStreamer.scale=$UI/StreamerPlaceholder.scale
 	currentStreamer.init(currentStreamerIndex, Global.currentStreamIndex)
