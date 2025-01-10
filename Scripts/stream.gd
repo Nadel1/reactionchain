647bbfcbcc497caf7da82bc2e2eda32b4e4ae0b9
Natalia <n.piotrowski@tum.de>
2025-01-10 16:33:12 +0100

    streamer deterioation

--- Scripts/stream.gd
+++ Scripts/stream.gd
@@ -32,8 +32,12 @@ func _on_start_playing_music_timer_timeout() -> void:
 	
 
 func prepareStreamer():
-	currentStreamerIndex=Global.currentStreamIndex%(allStreamers.size())
-			
+	if Global.currentStreamIndex<3:
+		currentStreamerIndex=randi()%2
+	elif Global.currentStreamIndex< 6:
+		currentStreamerIndex=randi()%2+2
+	else:
+		currentStreamerIndex=randi()%2+4
 	currentStreamer=allStreamers[currentStreamerIndex].instantiate()
 	currentStreamer.position=$UI/StreamerPlaceholder.position
 	currentStreamer.scale=$UI/StreamerPlaceholder.scale
